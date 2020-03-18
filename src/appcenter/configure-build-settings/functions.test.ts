a7061e0b672717ac5eac9369c11c4b6165841bf4
danStanciuLego <dan.stanciu@lego.com>
2020-03-18 13:20:27 +0100

    fix: fix test on PR

--- src/appcenter/configure-build-settings/functions.test.ts
+++ src/appcenter/configure-build-settings/functions.test.ts
@@ -2,14 +2,12 @@ import { FetchMock } from 'jest-fetch-mock';
 import { setBranchConfig } from './functions';
 import { expectedBody } from './testUtils';
 
-if (!process.env.GITHUB_REF) {
-  process.env.GITHUB_REF = 'refs/heads/feature/branchName';
-}
-
-// eslint-disable-next-line no-console
-console.log('process.env', process.env);
 const fetchMock: FetchMock = global.fetch;
-const currentBranchName = encodeURIComponent(process.env.GITHUB_REF?.split('refs/heads/')[1]);
+let branchName = process.env.GITHUB_REF?.split('refs/heads/')[1];
+if (!branchName) {
+  branchName = process.env.GITHUB_HEAD_REF;
+}
+const currentBranchName = encodeURIComponent(branchName);
 
 afterEach(() => {
   fetchMock.resetMocks();
