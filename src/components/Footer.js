667898c7338e78b6c4b52937a6afbe4ba2edf705
Andreas Hejndorf <andreas@hejndorf-foto.dk>
2021-01-15 17:42:44 +0100

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,9 @@ const Footer = () => {
         <div className="container-fluid footer">
             <div className="container">
                 <div className="row py-2">
-                    <div className="col">
-                        This ranking software was brought to you by Andreas Hejndorf
-                    </div>
-                    <div className="col">
-                        I am the second column
+                    <div className="col text-center">
+                        This ranking software was brought to you by Andreas Hejndorf<br />
+                        If you experience any problems, feel free to contact <a href="mailto:andreas@hejndorf-foto.dk">andreas@hejndorf-foto.dk</a>
                     </div>
                 </div>
             </div>
