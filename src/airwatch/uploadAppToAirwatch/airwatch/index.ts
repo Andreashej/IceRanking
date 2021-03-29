72a8136869355c7d7066b1dcd08ec139f20114b8
Matea27 <mhron1234@gmail.com>
2021-03-29 14:22:21 +0200

    fix: testing cdn

--- src/airwatch/uploadAppToAirwatch/airwatch/index.ts
+++ src/airwatch/uploadAppToAirwatch/airwatch/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { getAppDetails, logInfo } from '../../../utils';
+import { getAppDetails, getAppDetailsCDN, logInfo } from '../../../utils';
 import { awAssignSmartgroup } from './awAssignSmartgroup';
 import { awBeginInstall } from './awBeginInstall';
 import { awUploadBlob } from './awUploadBlob';
@@ -51,3 +51,47 @@ export const uploadToAirwatch: () => Promise<void> = async () => {
     throw error;
   }
 };
+export const uploadToAirwatchResign: () => Promise<void> = async () => {
+  try {
+    const { AD_GROUP } = process.env;
+    if (!AD_GROUP) {
+      throw new Error('Missing environment variables');
+    }
+
+    logInfo('Getting app details from the .ipa file');
+    const { version, bundleId, fileName, appName } = await getAppDetailsCDN();
+
+    logInfo('Getting previous app versions from Airwatch');
+    let previousApps = await getAppDetailsFromAW(bundleId);
+    if (previousApps === undefined) {
+      previousApps = [];
+    }
+    const activeApps = previousApps.filter((app) => app.Status === 'Active');
+    const smartGroupIds = await getSmartGroupIds(
+      AD_GROUP,
+      bundleId,
+      version,
+      previousApps,
+      activeApps
+    );
+    logInfo('Starting to upload the app to Airwatch');
+    const blobId = await awUploadBlob(fileName);
+
+    logInfo('Starting installing the app in Airwatch');
+    const awAppId = await awBeginInstall(blobId, bundleId, version, appName);
+
+    logInfo('Starting app retirement');
+    await retireApp(activeApps);
+
+    if (previousApps.length === 0) {
+      logInfo('Assigning the smart group to the newly created app');
+
+      return await awAssignSmartgroup(awAppId, smartGroupIds);
+    }
+
+    return logInfo('Smart group is already assigned to the app');
+  } catch (error) {
+    throw error;
+  }
+};
+//uploadToAirwatchResign()
