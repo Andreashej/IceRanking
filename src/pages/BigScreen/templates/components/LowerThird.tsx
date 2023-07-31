a6f4db676bf25257a5c3aae717aba186eb8184a2
Andreas Hejndorf <andreas@hejndorf-foto.dk>
2023-07-31 22:30:15 +0200

    Add color option "no armband" and replace spaces with dashes

--- src/pages/BigScreen/templates/components/LowerThird.tsx
+++ src/pages/BigScreen/templates/components/LowerThird.tsx
@@ -1,34 +1,51 @@
-import React, { AnimationEventHandler, CSSProperties } from 'react'
-import { Header } from './Header';
+import React, { AnimationEventHandler, CSSProperties } from "react";
+import { Header } from "./Header";
 
 type LowerThirdProps = {
-    onHidden?: () => void;
-    color?: string;
-    header: JSX.Element;
-    tag?: JSX.Element | string;
-    footer?: JSX.Element;
-    show?: boolean;
-    className?: string;
-    gridTemplateColumns?: CSSProperties['gridTemplateColumns']
-}
+  onHidden?: () => void;
+  color?: string;
+  header: JSX.Element;
+  tag?: JSX.Element | string;
+  footer?: JSX.Element;
+  show?: boolean;
+  className?: string;
+  gridTemplateColumns?: CSSProperties["gridTemplateColumns"];
+};
 
-export const LowerThird: React.FC<LowerThirdProps> = ({ children, onHidden, show, header, color = 'default', className, gridTemplateColumns = "1fr", footer, tag }) => {
-    const hide: AnimationEventHandler<HTMLDivElement> = (event) => {
-        if (event.animationName === 'scaleOut') {
-            onHidden?.();
-        }
+export const LowerThird: React.FC<LowerThirdProps> = ({
+  children,
+  onHidden,
+  show,
+  header,
+  color = "default",
+  className,
+  gridTemplateColumns = "1fr",
+  footer,
+  tag,
+}) => {
+  const hide: AnimationEventHandler<HTMLDivElement> = (event) => {
+    if (event.animationName === "scaleOut") {
+      onHidden?.();
     }
+  };
 
-    return (
-        <div className={`lower-third ${show ? 'show' : 'hide'} ${className}`} onAnimationEnd={hide}>
-            {tag && <div className="tag">{tag}</div>}
-            <Header headerContent={header} style={{ borderColor: `var(--${color})` }} />
-            <div className="extra" style={{ gridTemplateColumns: gridTemplateColumns }}>
-                {children}
-            </div>
-            {footer && <div className="footer">
-                {footer}
-            </div>}
-        </div>
-    )
-}
+  return (
+    <div
+      className={`lower-third ${show ? "show" : "hide"} ${className}`}
+      onAnimationEnd={hide}
+    >
+      {tag && <div className="tag">{tag}</div>}
+      <Header
+        headerContent={header}
+        style={{ borderColor: `var(--${color.replaceAll(" ", "-")})` }}
+      />
+      <div
+        className="extra"
+        style={{ gridTemplateColumns: gridTemplateColumns }}
+      >
+        {children}
+      </div>
+      {footer && <div className="footer">{footer}</div>}
+    </div>
+  );
+};
