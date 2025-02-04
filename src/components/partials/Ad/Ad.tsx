aabf159d5e4c6874e81195d529ffd3f8541cd48c
Andreas Hejndorf <andreas.hejndorf@lego.com>
2025-02-04 23:04:15 +0100

    remove nm2024 ad

--- src/components/partials/Ad/Ad.tsx
+++ src/components/partials/Ad/Ad.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import nmMobile from "./ads/nm_mobile.jpg";
-import nmDesktop from "./ads/nm_desktop.jpg";
+import { Button } from "primereact/button";
 
 export const Ad: React.FC = () => {
   return (
@@ -12,7 +11,7 @@ export const Ad: React.FC = () => {
         padding: 0,
       }}
     >
-      <a
+      {/* <a
         href="https://www.sporti.dk/ticket.php?e=116&v=e-i"
         target="_blank"
         rel="noreferrer"
@@ -27,22 +26,36 @@ export const Ad: React.FC = () => {
           src={nmMobile}
           alt="Come to the Nordic Championships 2024 in Herning"
         />
-      </a>
-      {/* <div style={{ 
-                background: 'rgba(36, 48, 94, .6)',
-                padding: '1.25rem'
-                // display: 'flex',
-                // justifyContent: 'center'
-            }}>
-                <h2 className='display-4 text-white'>Get your ad here!</h2>
-                <p className='lead text-white'>Be seen by thousands of Icelandic Horse interested people!</p>
-                <div className='d-flex align-items-center'>
-                    <Button label="Get in touch" className='p-button-rounded register-button' style={{ fontSize: "1.2rem"}} onClick={() => {
-                        window.location.href = 'mailto:andreas@hejndorf-foto.dk'
-                    }} />
-                    <small className='ml-3 text-white'>or write an email to <a className='text-white' href="mailto:andreas@hejndorf-foto.dk">andreas@hejndorf-foto.dk</a></small>
-                </div>
-            </div> */}
+      </a> */}
+      <div
+        style={{
+          background: "rgba(36, 48, 94, .6)",
+          padding: "1.25rem",
+          // display: 'flex',
+          // justifyContent: 'center'
+        }}
+      >
+        <h2 className="display-4 text-white">Get your ad here!</h2>
+        <p className="lead text-white">
+          Be seen by thousands of Icelandic Horse interested people!
+        </p>
+        <div className="d-flex align-items-center">
+          <Button
+            label="Get in touch"
+            className="p-button-rounded register-button"
+            style={{ fontSize: "1.2rem" }}
+            onClick={() => {
+              window.location.href = "mailto:andreas@hejndorf-foto.dk";
+            }}
+          />
+          <small className="ml-3 text-white">
+            or write an email to{" "}
+            <a className="text-white" href="mailto:andreas@hejndorf-foto.dk">
+              andreas@hejndorf-foto.dk
+            </a>
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
